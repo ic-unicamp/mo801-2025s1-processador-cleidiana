8852c371b40ca207e919abd9d4d4d635eaecf74f
Cleidiana <cleidiana.reis19@gmail.com>
2025-03-20 18:22:16 -0300

    correção para o run.sh funcionar na minha maquina (windowns)

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ if ! iverilog -o tb *.v; then
 fi
 
 cp test/teste$1.mem memory.mem
-./tb > saida.out
+vvp ./tb > saida.out
 sed -i '' '1,/^\*\*\*.*$/d' saida.out
 cp saida.out test/saida$1.out
 cp saida.vcd test/saida$1.vcd
