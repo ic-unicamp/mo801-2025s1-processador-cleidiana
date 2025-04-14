6cc2ed6e4395a9561807f54074cccd0d8de272a9
Cleidiana <cleidiana.reis19@gmail.com>
2025-04-14 18:45:40 -0300

    add testes ok

--- core.v
+++ core.v
@@ -42,7 +42,7 @@ assign  rs2 = instr[24:20];
 assign  funct7 = instr[31:25];
 assign immI = {{20{instr[31]}}, instr[31:20]};
 assign immS = {{20{instr[31]}}, instr[31:25], instr[11:7]};
-assign immB = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
+assign immB = {{19{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
 assign immU = {instr[31:12], 12'b0};
 assign immJ = {{11{instr[31]}}, instr[31], instr[19:12], instr[20], instr[30:21], 1'b0};
 
